906f6c95c4aa32be1866bd8a8611138d303b7853
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-13 21:16:29 +0530

    Create 3A Updating Variables.java

---
+++ 3A Updating Variables.java
@@ -0,0 +1,22 @@
+// variables
+
+class Variables {
+    public static void main(String[] args){
+        int passangers = 0;
+        System.out.println(passangers); // S must capital in System
+        // at bus stop 1 
+        // update the variable;
+        
+        passangers += 9; // passangers = passangers+9
+        
+        System.out.println(passangers);
+        
+        passangers -= 3;
+        
+        System.out.println(passangers);
+        
+    }
+}
+
+// convention: lowerCamelCase
+// int variable = anotherVariable -- to assign other values

be89cfd045c631dd0c3763f6b80cd5551c93e6ad
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-13 21:33:41 +0530

    Create 4A String Type.java

---
+++ 4A String Type.java
@@ -0,0 +1,21 @@
+// Type: String
+
+class TypeString{
+    public static void main(String[] args){
+        String line = "I am a boy.";
+        String lineTwo = "My name is Subhranshu.";
+        System.out.println(line);
+        System.out.println(line+lineTwo);
+        String concat = line + lineTwo + "Its true.";
+        System.out.println(concat);
+        
+        // update the string
+        
+        line = "I am a girl.";
+        System.out.println(line);
+        
+    }
+}
+
+// be careful not to name the class as "String"
+// use whitespace for nice outputs

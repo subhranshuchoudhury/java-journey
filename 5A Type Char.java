ea48db55fd2e4649826d4b2a07b7ce6cc70477c2
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-13 22:26:57 +0530

    Create 5A Type Char.java

---
+++ 5A Type Char.java
@@ -0,0 +1,25 @@
+// char type
+
+class TypeChar{
+    public static void main(String[] args){
+        char gender = 'M';
+        // char name = 'Chintu'; error
+        // char gender = "M";
+        System.out.println("gender : " + gender);
+    }
+}
+
+// use single quote for character. - 'a';
+// use double quote for string . - "Subhranshu Choudhury.";
+
+
+// Why not use String always ??
+
+/*
+
+char consumes less memory
+char is faster than string
+string is an object
+char is a primitive data type
+
+*/

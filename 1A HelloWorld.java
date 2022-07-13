2410f375c8fc75c445b9630432de85db2757878f
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-13 19:47:45 +0530

    Create 1A HelloWorld.java

---
+++ 1A HelloWorld.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+// public keyword is not important bcz we have only one class
+// semicolon is most important ;
+// args can be anything - xyz..
+// javac HelloJava.java to compile the code file
+// java HelloJava to run the code file
+// always compile code to update the code you overwrite
+// filename should same as the main class name
+// here the filename should "subhranshu.java"
+// every class needs to be in CamelCase format
+// println() prints each msg on a new line

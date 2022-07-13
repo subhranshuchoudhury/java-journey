1d34e90cced5527ea33e2979e211c16b487f5141
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-13 20:20:10 +0530

    Create 2A Variables.java

---
+++ 2A Variables.java
@@ -0,0 +1,14 @@
+// variables
+
+class Variables {
+    public static void main(String[] args){
+        int people = 20;
+        int seatOnBus = 30; // lowerCamelCase
+        
+        System.out.println("Free Space:" + (seatOnBus-people));
+        
+    }
+}
+
+// convention: lowerCamelCase
+// int variable = anotherVariable -- to assign other values

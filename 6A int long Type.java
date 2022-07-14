cac0f71895766ec5cb646ddcf95fbccc88a2bda1
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-14 09:30:15 +0530

    Create 6A int long Type.java

---
+++ 6A int long Type.java
@@ -0,0 +1,22 @@
+// type int and long
+
+class IntLong {
+    public static void main(String[] args){
+        
+        int noOfFans = 5000;
+        System.out.println("Total no of fans: " + noOfFans + ".\n");
+        
+        long noOfPeople = 7000000000L; // L in the end of the number.
+        System.out.println("People live on earth: " + noOfPeople+ ".");
+        
+    }
+}
+
+
+// int can store from -ve 2 billion -2147483648 to +ve 2 billion 2147483647
+
+// int (4 byte)
+// long (8 byte)
+
+// int less memory than long.
+

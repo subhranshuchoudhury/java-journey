d726cfe9144bd8ae66f8e8f7d33675d0fd125248
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-14 12:58:34 +0530

    Create 9A Math Operators P2.java

---
+++ 9A Math Operators P2.java
@@ -0,0 +1,33 @@
+// Math Operators P-II
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        int num = 10;
+        int num2 = 3;
+       
+       // %
+        
+        System.out.println(num%num2); // op - 1 how? (3x3+1)
+        
+        // ++
+        
+        num++;
+        System.out.println(num); // op - 11
+        
+        // --
+        
+        num--;
+        System.out.println(num); // op - 10
+        
+        // +=
+        
+        num2 += num;
+        System.out.println(num2); // op - 13
+        
+        // -=
+        num2 -= num2;
+        System.out.println(num2); // op - 0
+    }
+}
+

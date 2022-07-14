0f8fa16ecc80da24a6451ba84511098fb0efdaca
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-14 09:52:34 +0530

    Create 7A double.java

---
+++ 7A double.java
@@ -0,0 +1,21 @@
+// double
+
+class decimals {
+    public static void main(String[] args) {
+        
+        double  PI = 3.1415;
+        int num = 12;
+        int num2 = 7;
+        System.out.println("The value of PI: " + PI + "\n");
+        
+        System.out.println("int/int returns int:(12/7) " + num/num2);
+        
+        
+        // double Num = 1; You can store int value in double but not vice versa.
+        // int Num2 = 1.1; error. You cant store.
+    }
+}
+
+// use double for math calculation.
+// avoid using int and long for math calculations.
+// double - used to store decimal values.

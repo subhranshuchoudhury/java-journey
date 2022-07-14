90a4849d1bc4be19f31f5d6eb8ad32c59ad321d5
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-14 13:09:44 +0530

    Create 1B Type Casting.java

---
+++ 1B Type Casting.java
@@ -0,0 +1,14 @@
+// Type casting
+
+class TypeCast{
+    public static void main(String[] args){
+        
+        double decimal = 4.367;
+        
+        // int integer = decimal; error.
+        
+        int integer  = (int)decimal; // this is called type casting
+        
+        System.out.println(integer); // op - 4
+    }
+}

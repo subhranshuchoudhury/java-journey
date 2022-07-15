f5b0a8ffee93316e9d1665276ec0436e8d0f5cf8
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-15 12:15:29 +0530

    Update B3 Scanner ChatBOT.java

--- B3 Scanner ChatBOT.java
+++ B3 Scanner ChatBOT.java
@@ -12,15 +12,20 @@ class ChatBot{
         
         System.out.println("Hello, What is your name?");
         name = scan.nextLine();
+        
         System.out.println("Hello " + name +" ,I am Rob, where are you from ?");
         place = scan.nextLine();
+        
         System.out.println("Nice i will add " + place + " into my bucket list to visit.\nHow old are you?");
         age = scan.nextInt();
-        scan.nextLine(); // resolve error
+        
         System.out.println("You are only "+age+ ". My age is "+(400/age)+" times of yours age. Ridiculous!! :D\nWhich Language you prefer for coding??");
+        scan.nextLine(); // resolve error(catch a void), delimiter
         Planguage = scan.nextLine();
         scan.close();
+        
         System.out.println( ""+Planguage + "! that's great..!! i know " +Planguage+ " pretty well.. i guess :)\nReally nice to chat with you... Thanks a lot.");
+        
         
     }
 }

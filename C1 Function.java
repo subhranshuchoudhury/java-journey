65af94443a6bd043634c670601305dd1efaafdec
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-17 00:12:59 +0530

    Create C1 Function.java

---
+++ C1 Function.java
@@ -0,0 +1,31 @@
+// Functions
+
+class Functions {
+    public static void main(String[] args) {
+         
+        printer();
+        
+    }
+    public static void printer(){
+            System.out.println("Hello, World!");
+        }
+}
+
+/*
+
+public(1) void(2) name(3)(int(4) param(5)){
+    // code
+}
+
+1: Level of access
+2: Return value type
+3: Function name
+4: Parameters type
+5: Parameters
+
+call your function to execute it: function_name();
+example: name(); you can also pass parameters.
+
+naming: harryPotter, subhranshuChoudhury -- bad examples
+
+*/

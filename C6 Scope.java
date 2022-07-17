d58fd53b40cdbe712425eb46ac8399cb4a937de1
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-17 23:18:03 +0530

    Create C6 Scope.java

---
+++ C6 Scope.java
@@ -0,0 +1,19 @@
+// Scope
+
+class LearnScope {
+    int banana = 5;// can be access anywhere in the LearnScope class
+    public static void main(String[] args){
+        int apple = 3; // only access inside main class
+        System.out.println(banana); // no error
+        System.out.println(apple);// no error
+        System.out.println(mango);// error
+        
+    }
+    
+    public static void accesTest(){
+        int mango = 4;
+        System.out.println(apple);// error
+        System.out.println(banana); // no error
+        System.out.println(mango);// no error
+    }
+}

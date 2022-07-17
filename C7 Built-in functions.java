9a1eb9edb532b6e8d51f164e4c6f3bc87291eeff
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-18 00:10:39 +0530

    Create C7 Built-in functions.java

---
+++ C7 Built-in functions.java
@@ -0,0 +1,20 @@
+// built-in java function
+
+class BuitinFinction {
+    public static void main(String[] args) {
+        
+        double random = Math.random();
+        System.out.println("Random value is: " + random);
+        
+        double power = Math.pow(2,2);
+        System.out.println("Power value is: " + power);
+         
+        double sine = Math.sin(1);
+        System.out.println("Sine value is: " + sine);
+        
+        //many more already comes with your jdk.
+    }
+}
+
+// A good develper does not memorize code!
+// A good developer uses the internet

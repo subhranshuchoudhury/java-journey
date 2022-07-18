ce009ec69620c85aabe43c2164f2c0b3b4fe1d88
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-18 11:56:31 +0530

    Create D1 For loop.java

---
+++ D1 For loop.java
@@ -0,0 +1,16 @@
+// for loop
+public class Main
+{
+  public static void main (String[]args)
+  {
+
+    for (int i = 0; i < 10; i++)
+      {				// initialization condition increament/decreament
+	System.out.println ("Hello World");
+      }				// prints 10 times
+  }
+}
+
+// for loop - you know how many times a code needs to run.
+
+// while loop - its not clear how many times a code needs to run.

bb208cafae9eca3e45556812c2d0340b3d200463
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-18 12:05:55 +0530

    Create D2 For loop practice.java

---
+++ D2 For loop practice.java
@@ -0,0 +1,17 @@
+// for loop
+public class Main
+{
+  public static void main (String[]args)
+  {
+
+    for (int i = 99; i > 0; i--)
+      {				// initialization condition increament/decreament
+	System.out.println (i + " bottles of beer on the wall, " + i + " bottles of beer!");
+	System.out.println("take one down, pass it around, " + (i-1) + " bottles of beer on the wall!");
+      }				
+  }
+}
+
+// for loop - you know how many times a code needs to run.
+
+// while loop - its not clear how many times a code needs to run.

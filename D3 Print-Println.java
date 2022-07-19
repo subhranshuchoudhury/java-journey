153c47f4e45c1b55e6b094e57efbec2c2dba90fa
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-19 09:47:05 +0530

    Create D3 Print-Println.java

---
+++ D3 Print-Println.java
@@ -0,0 +1,26 @@
+// print and println
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Subhranshu");
+		System.out.print("Choudhury");
+		System.out.print("Chintu");
+		System.out.println("Choudhury");
+		System.out.print("Smita");
+		
+/* Output
+	
+  Subhranshu
+  ChoudhuryChintuChoudhury
+  Smita
+        
+*/
+		
+		
+	}
+}
+
+// print: it wont move cursor to the next line.
+// println: move cursor to the next line.

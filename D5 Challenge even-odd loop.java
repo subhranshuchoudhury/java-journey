7eaf55ea1b7d0532c1a3dcbf9c3dfa7504df17e0
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-19 10:15:03 +0530

    Create D5 Challenge even-odd loop.java

---
+++ D5 Challenge even-odd loop.java
@@ -0,0 +1,19 @@
+// challenge (5.6)
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    System.out.println("1 to 19 Even or Odd: ");
+	    
+	    for (int i=0;i<20;i++){
+	        if(i%2==0){
+	            System.out.println(i + " -- " + "even");
+	        }else{
+	            System.out.println(i + " -- " + "odd");
+	        }
+	    } 
+	}
+}

8c527724e8cee5cd49ff8ba0184fb01d1085a3cb
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-21 10:27:49 +0530

    Create E3 Array.java

---
+++ E3 Array.java
@@ -0,0 +1,27 @@
+// Intro to Array
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        String[] Aliens = {"Alien-X","SwampFire","HeatBlast","WildMuch"};
+        
+        for(int i=0;i < Aliens.length; i++){
+            System.out.println("Alien Name: " + Aliens[i]);
+        }
+       
+    }
+}
+
+/*
+ * int[] integers = {1,2,3};
+ * String[] words = {"jane","john","chintu"};
+ * double[] decimals = {1.2,2.2,3,3};
+ * char[] letters = {'a','b','c'};
+ * words[0] = jane 1st index is -- 0
+ * words[2] = chintu
+ * array.length --> will give us the length of the array.
+ */
+
+
+
+

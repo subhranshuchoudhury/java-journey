7fef386c9ecc70409eb333f5a0a571c2c7d819fe
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-22 09:44:59 +0530

    Update and rename E8 2D array practice.java to E8 2D Array Loop.java

--- E8 2D Array Loop.java
+++ E8 2D Array Loop.java
@@ -1,7 +1,7 @@
 
 import java.util.Arrays;
 
-// 2D Array Practice.
+// 2D Array Loop.
 
 public class HelloWorld {
     
@@ -14,9 +14,11 @@ public class HelloWorld {
             {"Beverage: ", "2.44","4.5","6.7","3.2","1.2"},
             {"Cerals: ","12.99","8.99","3.45","3.2","1.2"}
         };
+        
+        // looping the array.
 
-        for (int i = 0; i < priceTable.length; i++) {
-            for (int j = 0; j < priceTable[i].length; j++) {
+        for (int i = 0; i < priceTable.length; i++) { // i refers to row.
+            for (int j = 0; j < priceTable[i].length; j++) { // j refers to column.
                 System.out.print(priceTable[i][j]  + "\t");
             }
             System.out.println();

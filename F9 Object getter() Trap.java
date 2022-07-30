d5522743538634dd8126bfed458f8c1870167b28
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-07-30 21:21:59 +0530

    Update F9 Object getter() Trap.java

--- F9 Object getter() Trap.java
+++ F9 Object getter() Trap.java
@@ -1,4 +1,16 @@
-public String[] getPassport(){
+import java.util.Arrays;
+
+public class Person {
+
+    String name;
+    String nationality;
+    String dateOfBirth;
+    String[] passport;
+    int seatNumber;
+
+    public String[] getPassport(){
         return Arrays.copyOf(this.passport, this.passport.length);
         // never: return this.passport --> it will change the value if you accidentaly change it.
+    }
+  
 }

dda82ce966741c26bcb424556a3acd17e9ba6419
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-11-08 13:24:06 +0530

    Update 2A Variables.java

--- 2A Variables.java
+++ 2A Variables.java
@@ -2,6 +2,10 @@
 
 class Variables {
     public static void main(String[] args){
+        int mark,age;
+        String name = "Subhranshu";
+        double salary = 8383.8;
+        // also float avalible with F or f in last.
         int people = 20;
         int seatOnBus = 30; // lowerCamelCase
         
